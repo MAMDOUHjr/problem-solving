c8b2281528ce2aba0ea59a1930492c904c0d4437
Amr Mamdouh <devmamdouhjr@gmail.com>
2024-09-01 14:19:57 +0300

    commit 1

---
+++ codeforces/A/58A - Chat room.cpp
@@ -0,0 +1,37 @@
+/*
+    Author : Amr Mamdouh 
+    Date : 1 sep 2024
+    cfURL: https://codeforces.com/problemset/problem/58/A
+
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s , s2  ; cin >> s; 
+    set<char> sc ;
+    for (int i = 0 ; i < s.size();i++){
+        sc.insert(s[i]);
+    }
+    s2 = "hello";
+    int j = 0 ;
+    for (int i = 0 ; i < s.size();i++){
+        if(s[i] == s2[j]){
+            j++;
+        }
+    }
+    if(j == 5){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+    
+    return 0;
+}
